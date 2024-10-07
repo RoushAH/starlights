658f4069f8e0b0f12544b17caea03a87505b348c
Aroush <roush.adam.h@gmail.com>
2024-10-07 15:13:04 +0100

    Beginning the attempt at auto-update

---
+++ code_update_retrieve.py
@@ -0,0 +1,51 @@
+import urequests
+import network
+import os
+
+
+def do_connect(wifi_num):
+    wifis = [("RET - IOT", "UwolnicMajonez!"), ("pretty-fly-for-a-wifi", "ExtrAT3st"), ("Bapunzel", "SayUpBig")]
+    network_choice = wifi_num
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(*wifis[network_choice])
+
+
+def do_test():
+    # target = "https://github.com/f3db02a9-553a-4ee3-bb16-f48c78f182f4"
+    target = "https://introcs.cs.princeton.edu/python/14array/sample.py"
+
+    r = urequests.get(target)
+    print(f"Response status code: {r.status_code}")
+    print("GREAT SUCCESS") if r.status_code == 200 else print("Whomp, whomp")
+    tgt_file = target.split("/").pop()
+    with open(tgt_file, "w") as results_out:
+        results_out.write(r.text)
+
+    print(f"Is {tgt_file} in memory? {tgt_file in os.listdir()}")
+    line = f"import {tgt_file[:-3]}"
+    print(f"Attempting to execute `{line}`")
+    try:
+        exec(line)
+    except Exception as e:
+        print(f"Failed, due to \n {e}")
+
+    print("Donezo")
+
+
+def do_update_all():
+    # Get the name of every file in the directory, and update them all
+    # Currently risky. UNTESTED
+    REMOTE_ACCESS = "http://url.for.remote.repo"
+    if REMOTE_ACCESS[-1] != "/":
+        REMOTE_ACCESS = REMOTE_ACCESS + "/"
+
+    for file in os.listdir(""):
+        r = urequests.get(REMOTE_ACCESS + file)
+        if r.status_code == 200:
+            with open(file, "w") as f:
+                f.write(r.text)
+
+
+do_connect(0)
+do_test()
